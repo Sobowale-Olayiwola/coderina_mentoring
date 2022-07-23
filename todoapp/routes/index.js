99903ae02043220cec7b066ca81a59110c405f42
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-07-23 16:15:47 +0100

    feat: added middlewares

--- todoapp/routes/index.js
+++ todoapp/routes/index.js
@@ -1,6 +1,7 @@
 const todoRouteHandler = require("./todo.routes");
 const userRouteHandler = require("./user.routes");
+const { verifyUserToken } = require("../middlewares/auth");
 module.exports = (app) => {
-  app.use("/api/v1/todos", todoRouteHandler);
+  app.use("/api/v1/todos", verifyUserToken, todoRouteHandler);
   app.use("/api/v1/users", userRouteHandler);
 };
