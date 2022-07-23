b69a855f60088530bd1382e85fca1f49ce30d214
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-07-23 16:22:13 +0100

    refactor: use env variable from process

--- todoapp/index.js
+++ todoapp/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { connectDB } = require("./models");
 const routes = require("./routes");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Connection to MongoDB
 connectDB();
